2868a38be0a5a7541d0970d27cbab9b40b5f0321
phamngoctrungnhan-jay04 <phamngoctrungnhan0901@gmail.com>
2025-05-18 13:27:39 +0700

    Lan thu 2 lam chuyen ay

--- MathUtilLive.java
+++ MathUtilLive.java
@@ -1,7 +1,11 @@
 public class MathUtilLive {
     public static void main(String[] args) {
-        long result = MathUtil.getFactorial(-5);
+        long result = MathUtil.getFactorial(5);
         System.out.println("5! = " + result);
+
+    //THÊM CODE SAU LẦN ĐẦU TIÊN LÀM CHUYỆN ẤY - CODE LÊN SERVER 13:26 18/5/2025
+    System.out.println("expected: 1! = 1; actual: " + MathUtil.getFactorial(1));   
+    System.out.println("expected: 3! = 6; actual: " + MathUtil.getFactorial(3));
     }
     
 }
